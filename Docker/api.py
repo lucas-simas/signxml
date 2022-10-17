32e2eb3fb742059480d40a30f9651a97e9717e83
Lucas <lucas@bgstudios.com.br>
2022-10-17 14:38:24 -0300

    Consertando retorno de binary para string

--- Docker/api.py
+++ Docker/api.py
@@ -33,12 +33,12 @@ class Assinar_XML_IPC_BRASIL(Resource):
 			signed_root = signxml.XMLSigner(method=signxml.methods.enveloped, digest_algorithm="sha1", signature_algorithm="rsa-sha1", c14n_algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315").sign(root, key=certkey, cert=cert)
 			# verified_data = XMLVerifier().verify(signed_root, ca_pem_file=cert).signed_xml
 
-			xmlstr = etree.tostring(signed_root, encoding='utf8', method='xml')
+			xmlstr = etree.tostring(signed_root, encoding='utf8', method='xml', pretty_print=True)
 
 			# et = etree.ElementTree(signed_root)
 			# et.write('output.xml', pretty_print=True)
 
-			return jsonify({'status': True, 'code': None, 'data': {'xml': xmlstr}})
+			return jsonify({'status': True, 'code': None, 'data': {'xml': xmlstr.decode('utf8')}})
 		except Exception as e:
 			return jsonify({'status': False, 'message': 'Erro encontrado em: ' + str(e), 'code': None, 'data': []})
 
