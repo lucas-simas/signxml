ae145559b2ee626e07833948500e0de9a1cd3e65
Lucas <lucas@bgstudios.com.br>
2022-10-17 11:51:28 -0300

    Melhorias para API e funcionamento do Docker

--- Docker/api.py
+++ Docker/api.py
@@ -30,7 +30,7 @@ class Assinar_XML_IPC_BRASIL(Resource):
 			certkey = certkey.read()
 			
 			root = etree.fromstring(xml)
-			signed_root = signxml.XMLSigner(method=signxml.methods.enveloped, digest_algorithm="sha1", signature_algorithm="rsa-sha1", c14n_algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315").sign(root, key=key, cert=cert)
+			signed_root = signxml.XMLSigner(method=signxml.methods.enveloped, digest_algorithm="sha1", signature_algorithm="rsa-sha1", c14n_algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315").sign(root, key=certkey, cert=cert)
 			# verified_data = XMLVerifier().verify(signed_root, ca_pem_file=cert).signed_xml
 
 			xmlstr = etree.tostring(signed_root, encoding='utf8', method='xml')
@@ -50,4 +50,4 @@ api.add_resource(Assinar_XML_IPC_BRASIL, '/xml/assinar')
 
 #Estas portas / host podem ser sobreescritos ao rodar em CMD
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='6002')
+	app.run(host='0.0.0.0', port='6025')
